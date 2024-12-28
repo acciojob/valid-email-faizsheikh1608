7d50c21f333296dba834057a2cdb3a5cd41531a6
faizsheikh1608 <164844564+faizsheikh1608@users.noreply.github.com>
2024-12-28 17:53:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 function validEmail(str) {
   //your JS code here.
+  if(str.includes('@')) return false;
+
+	const arr = str.split('@');
+	arr.forEach((curr) => {
+		if(curr.includes('@')) return false;
+	})
+
+	const localPart = arr[0];
+	const domainPart = arr[1];
+
+	//checking localpart
+	if(!localPart || localPart.includes('.') ) return false;
+
+	//checking domain
+	const domainArr = domainPart.spit('.');
+
+	 
+  if (domainParts.length < 2) return false;
+	
+	for(let part of domainArr){
+		if(!part) return false;
+	}
+
+
+	const tld = domainArr[domainArr.length - 1];
+	if(tld.length < 2 || tld > 3) return false
+
+	
+	return true;
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
+const str = prompts("Enter an email address.");
 alert(validEmail(str));
